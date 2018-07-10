7d0539901df5342b1b6d151b16d19487801e9d5f
Vince Mitchell <vince.mitchell@resultdata.com>
2018-07-10 10:29:05 -0400

    inital package setup

---
+++ src/AdsToolsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ResultData\ADSTools;
+
+use Illuminate\Support\ServiceProvider;
+
+class AdsToolsServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
